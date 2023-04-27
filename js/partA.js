746ced6e7b6c18e70f5bcea66bf97ebce34a6a15
Alexthipp <al394808@uji.es>
2023-04-27 15:40:51 +0200

    update

--- js/partA.js
+++ js/partA.js
@@ -1,3 +1,14 @@
+const HUD_HEIGHT = 50;
+const ROCKETS_GROUP_SIZE = 40;
+const ROCKETS_OFFSET_Y = 10;
+const ROCKETS_VELOCITY = 500;
+const ENEMIES_GROUP_SIZE = 200;
+const TIMER_RHYTHM = 0.1 * Phaser.Timer.SECOND;
+const NUM_LEVELS = 3;
+const LEVEL_ENEMIES_PROBABILITY = [0.2, 0.4, 0.6, 0.8, 1.0];
+const LEVEL_ENEMIES_VELOCITY = [50, 100, 150, 200, 250];
+const HITS_FOR_LEVEL_CHANGE = 50;
+
 let playAState = {
     preload: preloadPartA,
     create: createPartA
@@ -8,5 +19,41 @@ function preloadPartA(){
 }
 
 function createPartA(){
+    
+    //EN UN FUTURO HABRA QUE CREAR CLASES DE ESTO PARA USARLO EN TODAS LAS PARTES
+    //CLASES:
+    //CHARACTER
+    createCharacter();
+    createKeyControls();
+    //ROCKET
+    createRocketBullet(ROCKETS_GROUP_SIZE);
+    createSounds();
+    //ENEMIE
+    createEnemies(ENEMIES_GROUP_SIZE);
+    //HUD
+    createHUD();
+}
+
+function createCharacter(){
+
+}
+
+function createKeyControls(){
+
+}
+
+function createRocketBullet(number){
+
+}
+
+function createSounds(){
+
+}
+
+function createEnemies(number){
+
+}
+
+function createHUD(){
 
 }
